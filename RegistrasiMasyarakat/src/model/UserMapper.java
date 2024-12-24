900998c98bde3c23607c57907e763b289ad57ce6
Lyramor <mmarsa2435@gmail.com>
2024-12-25 00:30:43 +0700

    [UPDATE] Halaman User Profile

--- RegistrasiMasyarakat/src/model/UserMapper.java
+++ RegistrasiMasyarakat/src/model/UserMapper.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+package model;
 
 /**
  *
  * @author m marsa n j
  */
-public class UserView {
+public class UserMapper {
     
 }
